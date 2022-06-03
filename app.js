f8246b67d1deb330ac1bb4f34dee5f56befc4927
la-viza <97736137+la-viza@users.noreply.github.com>
2022-06-03 15:24:56 +0300

    Working on eventListeners in JS

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
 let ratings = document.querySelectorAll(".rating");
 let submitBtn = document.querySelector(".card__front__btn");
 let sections = document.querySelectorAll("section");
-let number = document.querySelector("span")
+let ratingNumber = document.querySelector("span");
+let ratingValue = 0;
+
+const toggleRating = e => {
+    let ratingSelected = e.target
+    if (ratingSelected.classList.contains("selected")) {
+        ratingSelected.classList.remove("selected")
+        submitBtn.classList.add("unHovered")
+        ratingValue=0
+        return
+    }
+
+ratings.forEach ((rating) => {
+if (rating.classList.contains ("selected")) {
+    rating.classList.remove("selected")
+}
+})
+
+ratingSelected.classList.add("selected")
+ submitBtn.classList.add("unHovered")
+ ratingValue = selectedRating.innertext
+ return
+}
+
+const toggleRating = () => {
+    sections[0].classList.replace("main", "hidden")
+    sections[1].classList.replace("hidden", "main")
+    ratingNumber.innerText=ratingValue
+}
+
+
+
+
+
+
