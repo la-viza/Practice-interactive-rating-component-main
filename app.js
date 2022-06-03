cf93c48a684a23e1d24674edae6242f0a14cadce
la-viza <97736137+la-viza@users.noreply.github.com>
2022-06-03 15:08:16 +0300

    finished editing HTML elements

---
+++ app.js
@@ -0,0 +1,4 @@
+let ratings = document.querySelectorAll(".rating");
+let submitBtn = document.querySelector(".card__front__btn");
+let sections = document.querySelectorAll("section");
+let number = document.querySelector("span")
